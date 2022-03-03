089327f1ba893737823a0024df95d9be3c14f23e
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-03 14:43:35 -0500

    Complete js-learnings 01

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// declare name and admin
+let admin, name;
+
+name = "John";
+
+// assign admin to be name
+admin = name;
+
+//alert message
+//alert(admin);
+
+//declare name of planet
+let planetName = "Earth";
+
+//declare current visitor;
+let currentVisitor = "John";
+
+
+// exercise 1 + 2
+console.log(23 + 97);
+console.log(30 + 237);
+console.log(233 + 948);
+console.log(123 + 197);
+console.log(16 + 653);
+console.log(454 + 784);
+console.log(1 + 1);
+
+// exercise 3
+console.log((4 + 6 + 9) / 77);
+
+// exercise 4
+let a = 10;
+
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+//exercise 5 
+const max = 57;
+
+let actual = max - 13;
+let percentage = actual/max;
