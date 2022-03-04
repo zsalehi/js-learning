ac096e9e8ff029c755f1c5c2de7d876e897c1580
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-04 16:13:56 -0500

    Complete js-foundations-03

--- script.js
+++ script.js
@@ -43,3 +43,35 @@ const max = 57;
 
 let actual = max - 13;
 let percentage = actual/max;
+
+
+//js fundamentals part 3 - exercise 1
+function add7(num) {
+    return num + 7;
+}
+
+console.log(add7(9));
+
+//js fundamentals part 3 - exercise 2
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(5, 5));
+
+//js fundamentals part 3 - exercise 3
+
+function capitalize(str) {
+   return str[0].toUpperCase() + str.slice(1);
+}
+
+console.log(capitalize("zAIGHAM"));
+
+//js fundamentals part 3 - exercise 4
+
+function lastLetter(str) {
+    length = str.length;
+    return str.slice(length-1);
+}
+
+console.log(lastLetter("zaigham"));
